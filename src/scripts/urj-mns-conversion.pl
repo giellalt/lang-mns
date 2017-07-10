3ac0745ed3b0d2525301b5504ac54ef13a6b7b98
Trond Trosterud <trond.trosterud@uit.no>
2017-07-10 09:19:59 +0000

    Converter for urj-Cyrl lexica.

---
+++ src/scripts/urj-mns-conversion.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl -w
+#
+# urj-mns-conversion.pl
+# Convert names in the URJ propernoun lexicon to mns.
+
+use strict;
+use utf8;
+
+while(<>) {
+	# Continuation lexicon substitutions:
+#	s/ C-FI-NEN/nen LONDON/g ;
+	s/ PROP_KIT_FEM/ PROP/g ;
+	s/ PROP_LAK_MAL/ PROP/g ;
+	s/ PropNameMaleDer-Y-0Evich/ PROP/g ;
+	s/ PROP_OSH_SUR/ PROP/g ;
+	s/ Deriv-RUS-AN_SURMAL/ PROP/g ;
+	s/ CYRL-L_SUR/ PROP/g ;
+	s/ Deriv-RUS-IJ_SURMAL/ий PROP/g ;
+	s/ Deriv-RUS-YJ_SURMAL/ый PROP/g ;
+	s/ Deriv-RUS-KIJ_SURMAL/ий PROP/g ;
+	s/ Deriv-RUS-OJ_SURMAL/ой PROP/g ;
+	s/ CYRL-K_SUR/ PROP/g ;
+	s/ CYRL-SIBILANT_SUR/ PROP/g ;
+	s/ Deriv-RUS-IN_SURMAL/ PROP/g ;
+	s/ Deriv-RUS-V_SURMAL/ PROP/g ;
+	s/ CYRL-T_SUR/ PROP/g ;
+	s/ CYRL-CONS_SUR/ PROP/g ;
+	s/ CYRL-VOW_SUR/ PROP/g ;
+	s/ CYRL-A_SUR/ PROP/g ;
+	s/ PROP_OSH_MAL/ PROP/g ;
+	s/ PROP_KIT_MAL/ PROP/g ;
+	s/ PROP_KUDO_PATRFEM/ PROP/g ;
+	s/ PROP_KUDO_MAL/ PROP/g ;
+	s/ PropNameMaleDer-Ovich/ PROP/g ;
+	s/ PROP_OSH_PATRMAL/ PROP/g ;
+	s/ PropNameMaleDer-IJ-Y0Evich/ий PROP/g ;
+	s/ PropNameMaleDer-J-0Evich/й PROP/g ;
+	s/ PropNameMaleDer-IJ-I0Evich/ий PROP/g ;
+	s/ PropNameMaleDer-I-YEvich/и PROP/g ;
+	s/ PROP_KAL_MAL/ PROP/g ;
+	s/ PROP_KAL_FEM/ PROP/g ;
+	s/ PROP_OSH_FEM/ PROP/g ;
+	s/ PROP_KUDO_FEM/ PROP/g ;
+	s/ PROP-PLC_KIT/ PROP/g ;
+	s/ PROP-PLC_KAL/ PROP/g ;
+	s/ PROP-PLC_KUDO/ PROP/g ;
+	s/ PROP_KIT_SUR/ PROP/g ;
+	
+# loanwords with compound border over identical vowels
+	s/Hjarteelva/Hjarte-elva/g ;
+	
+
+# names with Inari Saami inflection
+	s/^Aanaar\+/!Aanaar+/g ;
+
+# sme special symbols
+	s/b9/b/g ;
+
+	my $line = $_;
+
+	print $line;
+}
+
