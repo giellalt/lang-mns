31c6f14660c23f50ad5802027e66f4ddafefee04
Trond Trosterud <trond.trosterud@uit.no>
2020-04-28 12:34:41 +0000

    ny sti med fst for morphology

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -2,15 +2,15 @@
 
 echo 'Are there tags not declared in root.lexc or misspelled?'
 cat  \
-    src/morphology/affixes/*lexc src/morphology/stems/*lexc \
-    src/morphology/affixes/*lexc src/morphology/affixes/*lexc \
-    src/morphology/affixes/*lexc src/morphology/generated-files/*lexc | \
+    src/fst/affixes/*lexc src/fst/stems/*lexc \
+    src/fst/affixes/*lexc src/fst/affixes/*lexc \
+    src/fst/affixes/*lexc src/fst/generated-files/*lexc | \
     cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | \
     cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'| \
     tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | \
     egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
-cat src/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+cat src/fst/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
 echo 'Have a look at these:'
 
